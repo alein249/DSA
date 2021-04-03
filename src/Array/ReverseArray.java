253ebf1437e928c1bd1dca1fa91c4db74348bada
Pankaj Sharma <panksh249@gmail.com>
2021-04-03 22:34:26 +0530

    Reverse array

--- src/Array/ReverseArray.java
+++ src/Array/ReverseArray.java
@@ -1,4 +1,18 @@
 package Array;
 
 public class ReverseArray {
+    public static void main(String[] args) {
+        //initialize array
+        int[] array = {2,3,4,5};
+        System.out.println("Original array: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Array in reverse order: ");
+        //Loop through the array in reverse order
+        for (int i = array.length-1; i >= 0; i--){
+            System.out.print(array[i] + " ");
+        }
+    }
 }
