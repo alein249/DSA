3a263dc57bb51badb4eef4715009928b3fbb3d03
Pankaj Sharma <panksh249@gmail.com>
2021-09-17 11:57:29 +0530

    Maximum element in Array using Recursion

---
+++ src/Recursion/MaximumArray.java
@@ -0,0 +1,29 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class MaximumArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = maxOfArray(arr, 0);
+        System.out.println(max);
+    }
+
+    private static int maxOfArray(int[] arr, int idx) {
+        if(idx == arr.length-1){
+            return arr[idx];
+        }
+        int misa = maxOfArray(arr, idx+1);
+        if(misa > arr[idx]){
+            return misa;
+        }
+        else {
+            return arr[idx];
+        }
+    }
+}
